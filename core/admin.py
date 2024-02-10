47ac6dddd8a83978a2eebdd68040be6985f62ddb
MuKeLaNGlo <steammike23@gmail.com>
2024-02-11 03:01:53 +0500

    еще причесал админку

--- core/admin.py
+++ core/admin.py
@@ -4,6 +4,12 @@ from django.utils.html import mark_safe
 from core import models
 
 
+@admin.register(models.CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    model = models.CustomUser
+    list_display = ('username', 'first_name', 'last_name', 'is_active', 'is_staff', 'date_joined')
+
+
 class SocialLinkInline(admin.TabularInline):
     model = models.SocialLink
     extra = 1
@@ -12,6 +18,7 @@ class SocialLinkInline(admin.TabularInline):
 @admin.register(models.UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'created_at', 'updated_at', 'get_image')
+    exclude = ('social_links',)
 
     def get_image(self, obj):
         if obj.avatar:
@@ -30,12 +37,12 @@ class SocialLinkAdmin(admin.ModelAdmin):
     list_display = ('user_profile', 'platform_name', 'link')
 
 
-@admin.register(models.AdminProfileProxy)
-class AdminProfileProxyAdmin(admin.ModelAdmin):
-    list_display = ('user', 'created_at', 'updated_at')
+# @admin.register(models.AdminProfileProxy)
+# class AdminProfileProxyAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'created_at', 'updated_at')
 
-    def get_queryset(self, request):
-        return super().get_queryset(request).filter(user__is_superuser=True)
+#     def get_queryset(self, request):
+#         return super().get_queryset(request).filter(user__is_superuser=True)
 
 
 @admin.register(models.Project)
@@ -55,7 +62,16 @@ class TaskAdmin(admin.ModelAdmin):
 
 @admin.register(models.TaskComment)
 class TaskCommentAdmin(admin.ModelAdmin):
-    list_display = ('task', 'user', 'text', 'parent_comment', 'created_at')
+    list_display = ('task', 'user', 'text', 'short_parent_comment', 'created_at')
+
+    def short_parent_comment(self, obj):
+        if len(str(obj.parent_comment)) > 40:
+            return str(obj.parent_comment)[:37] + '...'  # Обрезаем текст до 40 символов
+        elif obj.parent_comment:
+            return obj.parent_comment
+        return
+
+    short_parent_comment.short_description = 'Родительский комментарий'
 
 
 @admin.register(models.TaskMedia)
